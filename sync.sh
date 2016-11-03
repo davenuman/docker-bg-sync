7c2be15ee6b5f2a8147488fd4addfb463918ac34
David Numan <david.numan@civicactions.com>
2016-11-02 22:07:21 -0400

    Changing from file watching to waiting

--- sync.sh
+++ sync.sh
@@ -22,7 +22,7 @@ log_error_exit() {
 # Set defaults for all variables that we depend on (if they aren't already set in env).
 #
 
-# The source for the sync. This will also be recursively monitored by inotifywatch.
+# The source for the sync.
 : ${SYNC_SOURCE:="/source"}
 
 # The destination for sync. When files are changed in the source, they are automatically
@@ -33,9 +33,8 @@ log_error_exit() {
 # run by this script.
 : ${SYNC_VERBOSE:="0"}
 
-# If set, this script will attempt to increase the inotify limit accordingly.
-# This option REQUIRES that the container be run as a privileged container.
-: ${SYNC_MAX_INOTIFY_WATCHES:=''}
+# The wait time in between repeated unison run.
+: ${SYNC_WAIT:="30"}
 
 # This variable will be appended to the end of the Unison profile.
 : ${SYNC_EXTRA_UNISON_PROFILE_OPTS:=''}
@@ -48,9 +47,7 @@ log_heading "Configuration:"
 log_info "SYNC_SOURCE:                  $SYNC_SOURCE"
 log_info "SYNC_DESTINATION:             $SYNC_DESTINATION"
 log_info "SYNC_VERBOSE:                 $SYNC_VERBOSE"
-if [ -n "${SYNC_MAX_INOTIFY_WATCHES}" ]; then
-  log_info "SYNC_MAX_INOTIFY_WATCHES:     $SYNC_MAX_INOTIFY_WATCHES"
-fi
+log_info "SYNC_WAIT:                    $SYNC_WAIT"
 
 # Validate values as much as possible.
 [ -d "$SYNC_SOURCE" ] || log_error_exit "Source directory does not exist!"
@@ -72,33 +69,6 @@ fi
 # If verbose mode is off, add the --quiet option to rsync calls.
 if [[ "$SYNC_VERBOSE" == "0" ]]; then
   SYNC_RSYNC_ARGS="$SYNC_RSYNC_ARGS --quiet"
-fi
-
-if [ -z "${SYNC_MAX_INOTIFY_WATCHES}" ]; then
-  # If SYNC_MAX_INOTIFY_WATCHES is not set and the number of files in the source
-  # is greater than the default inotify limit, warn the user so that they can
-  # take appropriate action.
-  file_count="$(find $SYNC_SOURCE | wc -l)"
-  if [[ "$file_count" < "8192" ]]; then
-    log_heading "inotify may not be able to monitor all of your files!"
-    log_info "By default, inotify can only monitor 8192 files. The configured source directory"
-    log_info "contains $file_count files. It's extremely likely that you will need to increase"
-    log_info "your inotify limit in order to be able to sync your files properly."
-    log_info "See the documentation for the SYNC_MAX_INOTIFY_WATCHES environment variable"
-    log_info "to handle increasing that limit inside of this container."
-  fi
-else
-  # If bg-sync runs with this environment variable set, we'll try to set the config
-  # appropriately, but there's not much we can do if we're not allowed to do that.
-  log_heading "Attempting to set maximum inotify watches to $SYNC_MAX_INOTIFY_WATCHES"
-  log_info "If the container exits with 'Operation not allowed', make sure that"
-  log_info "the container is running in privileged mode."
-  if [ -z "$(sysctl -p)" ]; then
-    echo fs.inotify.max_user_watches=$SYNC_MAX_INOTIFY_WATCHES | tee -a /etc/sysctl.conf && sysctl -p
-  else
-    log_info "Looks like /etc/sysctl.conf already has fs.inotify.max_user_watches defined."
-    log_info "Skipping this step."
-  fi
 fi
 
 # Generate a unison profile so that we don't have a million options being passed
@@ -127,7 +97,7 @@ fastcheck=true
 maxthreads=10
 nodeletion=$SYNC_SOURCE
 prefer=$SYNC_SOURCE
-repeat=watch
+repeat=$SYNC_WAIT
 silent=$unisonsilent
 
 # Files to ignore
